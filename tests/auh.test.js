93c9762a1e11c89cd4a62a3546edce82e8c84568
HashimovKamran <kamranhesimov9@gmail.com>
2023-02-11 17:34:59 +0400

    Last changes

--- tests/auh.test.js
+++ tests/auh.test.js
@@ -9,11 +9,18 @@ const data = {
     }
 };
 
-describe('POST /api/v1/auth/register', () => {
+describe('POST /api/v1/auth', () => {
     it('should respond with JSON and status code 201', async () => {
       const response = await request(server).post('/api/v1/auth/register').send(data);
 
       expect(response.statusCode).toBe(201);
-      //expect(response.body).toHaveProperty('message');
+      expect(response.body).toHaveProperty('message');
+    });
+
+    it('should respond with JSON and status code 201', async () => {
+        const response = await request(server).post('/api/v1/auth/login').send(data);
+  
+        expect(response.statusCode).toBe(201);
+        expect(response.body).toHaveProperty('message');
     });
 });
